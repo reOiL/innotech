a6d11d4f004c84d0638f8885c5b16e7d2fb4353e
reOiL <imidg3001@yandex.ru>
2020-11-28 22:03:19 +0300

    Backend: Added upload form

--- pallada/pallada_app/views.py
+++ pallada/pallada_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from . import forms
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def upload(request):
+    if request.method != 'POST':
+        return JsonResponse({})
+    form = forms.UploadForm(request.POST)
+    if not form.is_valid():
+        return JsonResponse({'data': form.errors})
+    return JsonResponse({})
