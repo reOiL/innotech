f409eb08667bd0b8465c3bedea70a8cb46fcba68
reOiL <imidg3001@yandex.ru>
2020-11-28 14:00:59 +0300

    Backend: Added database for scrapped data

--- pallada/pallada_app/admin.py
+++ pallada/pallada_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from pallada_app import models
+
+
+@admin.register(models.ScrappedData)
+class ScrappedDataAdmin(admin.ModelAdmin):
+    pass
--- pallada/pallada_app/models.py
+++ pallada/pallada_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+
+class ScrappedData(models.Model):
+    class Meta:
+        verbose_name = _("Research data")
+        verbose_name_plural = _("Research data")
+
+    def __str__(self):
+        return str(self.id)
+
+    data = models.JSONField(
+        max_length=262144,
+        default=lambda: {},
+        null=False,
+        verbose_name=_("User data")
+    )
